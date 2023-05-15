6b0e4ef0ef08383a6357d93fc3959b4020af95a4
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-14 22:56:39 -0300

    feat: add header component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<div class="flex flex-wrap justify-left bg-green-500 w-full px-5 py-2 fixed top-0">
+    <div class="flex flex-row">
+        <img src="icons\header\cards.svg" class="h-12">
+        <div class="mx-auto my-5 px-5 font-bold text-white text-2xl" style="margin: auto 1px auto 1px; font-size: 30px;">
+            MTG Card Collection
+        </div>
+    </div>
+</div>
+
+@component('components.navbar')
+@endcomponent
