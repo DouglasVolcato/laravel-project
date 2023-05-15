bcf0839593b508207ce56ffd187eab92cad49489
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-14 22:55:51 -0300

    chore: implement footer styles

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,7 @@
-<footer>
-    <h3 class="underline" >Footer</h3>
-</footer>
+<div>
+    <div class="bg-green-600 flex justify-center text-white w-full h-auto fixed bottom-0">
+        <div class="font-bold text-white text-base my-1">
+            Made by <a class="underline" href="https://github.com/DouglasVolcato" target="blank">Douglas Volcato</a>
+        </div>
+    </div>
+</div>
