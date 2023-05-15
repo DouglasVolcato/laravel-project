342a286cbb2359dfcfe18bcf402653b518f3e2c7
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-14 22:55:20 -0300

    chore: implement navbar styles

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,10 @@
-<nav>
-    <button>Find Cards</button>
-    <button>My collection</button>
+<nav class="flex flex-wrap justify-center mb-3">
+    <button type="button"
+        class="border font-bold border-white-500 bg-blue-800 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-green-700 focus:outline-none focus:shadow-outline">
+        Find Cards
+    </button>
+    <button type="button"
+        class="border font-bold border-white-500 bg-green-800 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-blue-500 focus:outline-none focus:shadow-outline">
+        My collection
+    </button>
 </nav>
