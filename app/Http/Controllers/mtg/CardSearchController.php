b77d6513c7e389dcfe1a505519dc72d4581a0be0
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-16 23:55:28 -0300

    feat: implement card search parameters

--- app/Http/Controllers/mtg/CardSearchController.php
+++ app/Http/Controllers/mtg/CardSearchController.php
@@ -1,28 +1,96 @@
 <?php
+
 namespace app\Http\Controllers\mtg;
 
 use App\Http\Helpers\RequestSender\RequestSenderHelper;
+use App\Http\Requests\CardListSearchParametersRequest;
 use Illuminate\Routing\Controller as BaseController;
 
 class CardSearchController extends BaseController
 {
     private $requestSender;
-    private $apiLink;
 
     public function __construct()
     {
         $this->requestSender = new RequestSenderHelper();
-        $this->apiLink = 'https://api.magicthegathering.io/v1/cards';
     }
 
-    public function getCards($page = 1)
+    public function showCardListView(CardListSearchParametersRequest $request)
     {
-        $apiLink = "{$this->apiLink}?page={$page}";
+        $page = $request->page;
+        $name = $request->name;
+        $cmc = $request->cmc;
+        $colors = $request->colors;
+        $types = $request->types;
+        $supertypes = $request->supertypes;
+        $rarity = $request->rarity;
+        $text = $request->text;
+        $power = $request->power;
+        $toughness = $request->toughness;
+
+        $parameters = "?";
+
+        if (isset($page)) {
+            $parameters .= "page={$page}";
+        } else {
+            $parameters .= "page=1";
+        }
+
+        if (isset($name)) {
+            $parameters .= "&name={$name}";
+        }
+
+        if (isset($cmc)) {
+            $parameters .= "&cmc={$cmc}";
+        }
+
+        if (isset($colors)) {
+            $parameters .= "&colors={$colors}";
+        }
+
+        if (isset($types)) {
+            $parameters .= "&types={$types}";
+        }
+
+        if (isset($supertypes)) {
+            $parameters .= "&supertypes={$supertypes}";
+        }
+
+        if (isset($rarity)) {
+            $parameters .= "&rarity={$rarity}";
+        }
+
+        if (isset($text)) {
+            $parameters .= "&text={$text}";
+        }
+
+        if (isset($power)) {
+            $parameters .= "&power={$power}";
+        }
+
+        if (isset($toughness)) {
+            $parameters .= "&toughness={$toughness}";
+        }
+
+        if (strlen($parameters) > 7) {
+            $parameters .= "&pageSize=300";
+        } else {
+            $parameters .= "&pageSize=30";
+        }
+
+
+        $cards = $this->getCards($parameters);
+
+        return view('home', ['cards' => $cards]);
+    }
+
+    private function getCards($parameters)
+    {
+        $apiLink = 'https://api.magicthegathering.io/v1/cards' . $parameters;
         $data = $this->requestSender->get($apiLink);
 
         $cards = json_decode($data)->cards;
 
         return $cards;
     }
-
 }
