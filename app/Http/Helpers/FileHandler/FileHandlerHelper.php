0f0c1a21c09b221783ced1fe094cf7d566e6e84c
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-19 00:53:35 -0300

    fix: fix mocked data file path

--- app/Http/Helpers/FileHandler/FileHandlerHelper.php
+++ app/Http/Helpers/FileHandler/FileHandlerHelper.php
@@ -4,21 +4,26 @@ namespace App\Http\Helpers\FileHandler;
 
 class FileHandlerHelper{
     public function setContent($fileName, $content){
-        $filePath = "database\mocks{$fileName}";
+        $filePath = $this->setFilePath($fileName);
 
-        if(file_put_contents("database\mocks{$filePath}.txt", $content)){
+        if(file_put_contents($filePath, $content)){
             return true;
         } else {
             return false;
         }
     }
+
     public function getContent($fileName){
-        $filePath = "database\mocks{$fileName}";
+        $filePath = $this->setFilePath($fileName);
 
         if(!file_exists($filePath)){
             return false;
         } else {
             return file_get_contents($filePath);
         }
+    }
+
+    private function setFilePath($fileName){
+        return "mocks/{$fileName}.txt";
     }
 }
