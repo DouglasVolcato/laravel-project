646d223ba8757c8a37a78645c4fb0bfb85da1fe2
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-21 16:09:43 -0300

    chore: add button shadow

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route($route) }}"
     class="@php echo "border font-bold border-white-500 bg-".$color."-800
         text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-orange-800 focus:outline-none
-        focus:shadow-outline"; @endphp">{{ $label }}</a>
+        focus:shadow-outline shadow-2xl"; @endphp">{{ $label }}</a>
