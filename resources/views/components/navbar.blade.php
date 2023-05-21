255715869457e2a6293b7a038dae60dd61b1f014
Douglas Volcato <douglasvolcato@gmail.com>
2023-05-21 16:10:02 -0300

    feat: add card search navbar link

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,13 @@
 <nav class="flex flex-wrap justify-center mb-3">
     @component('components.button', [
         'type' => 'button',
+        'route' => 'cardList',
+        'color' => 'blue',
+        'label' => 'Search cards',
+    ])
+    @endcomponent
+    @component('components.button', [
+        'type' => 'button',
         'route' => 'cardRegistrationView',
         'color' => 'blue',
         'label' => 'Register card',
